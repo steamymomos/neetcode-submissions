5a2aeb45d1bfa60d5af2f3c3e64a9452b58a8052
steamymomos <97946559+steamymomos@users.noreply.github.com>
2026-06-28 16:17:00 +0530

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        deque<int> q;
+        vector<int> out(n-k+1);
+        int l=0,r=0;
+        while(r<n){
+            while(!q.empty() && nums[q.back()]<nums[r]){
+                q.pop_back();
+            }
+            q.push_back(r);
+            if(l>q.front()){q.pop_front();}
+            if((r+1)>=k){
+                out[l]=nums[q.front()];
+                l++;
+            }
+            r++;
+        }
+        return out;
+    }
+};
