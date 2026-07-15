fbe3142a4f79219c9019902af76c64766c8a3663
steamymomos <97946559+steamymomos@users.noreply.github.com>
2026-07-15 20:38:00 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fn(int n,vector<int>& dp){
+        if(n<0)return 0;
+        if(n==0) return 1;
+        if(dp[n]!=-1)return dp[n];
+        dp[n]=fn(n-1,dp)+fn(n-2,dp);
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return fn(n,dp);
+    }
+};
