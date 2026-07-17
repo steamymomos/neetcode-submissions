34f99c31fd5077632ad67d05ead6c22829db045e
steamymomos <97946559+steamymomos@users.noreply.github.com>
2026-07-17 22:14:13 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dfs(vector<int>& coins,vector<vector<int>>& dp,int amount,int i){
+        if(amount==0)return dp[0][i];
+        if(amount<0){
+            return 0;
+        }
+                if(i>=coins.size())return 0;
+        if(dp[amount][i]!=-1)return dp[amount][i];
+        return dp[amount][i]=dfs(coins,dp,amount-coins[i],i)+dfs(coins,dp,amount,i+1);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>> dp(amount+1,vector<int>(n+1,-1));
+        for(int i=0;i<n+1;i++)
+        dp[0][i]=1;
+        return dfs(coins,dp,amount,0);
+    }
+};
