6c28c3de73c72c3e6539d22c4c49de2e7cfa0470
steamymomos <97946559+steamymomos@users.noreply.github.com>
2026-07-17 11:50:43 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.length();
+        vector<bool> dp(n+1,false);
+        dp[n]=true;
+        for(int i=n-1;i>=0;i--){
+            for(auto w:wordDict){
+                if(i+w.length()<=n && s.substr(i,w.length())==w){
+                    dp[i]=dp[i+w.length()];
+                }
+                if(dp[i]){
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+};
