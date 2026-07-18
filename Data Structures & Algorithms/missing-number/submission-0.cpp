6d42e32745bd1cbcbe4cad2a6abb3a8158a29fd5
steamymomos <97946559+steamymomos@users.noreply.github.com>
2026-07-18 16:17:29 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+        int res=0;
+        for(int i=1;i<nums.size()+1;i++){
+            res^=i;
+        }
+        for(int num:nums){
+            res^=num;
+        }
+        return res;
+    }
+};
